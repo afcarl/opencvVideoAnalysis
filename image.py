bd6587dbb05cda72dc727a274b058fb91aff3158
Nishank Sharma <nishankatwork@gmail.com>
2017-07-08 20:15:25 +0530

    Add files via upload

---
+++ image.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+#                               =0
+img =cv2.imread('iphone.jpg',cv2.IMREAD_GRAYSCALE)
+#IMREAD_COLOR = 1
+#IMREAD_UNCHANGED = -1
+
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+#plt.imshow(img,cmap='gray',interpolation='bicubic')
+#plt.show()
+
+cv2.imwrite('watchgray.png',img)
